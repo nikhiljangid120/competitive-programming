af1fbe2e827d8fac19c2a6f7881799815e2ad4ad
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2026-09-15 00:29:38 +0530

    LeetCode: valid-anagram — solution file

---
+++ LeetCode/valid-anagram/solution.cpp
@@ -0,0 +1,19 @@
+1
+2
+3
+4
+8
+5
+6
+7
+9
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size()) return 0;
+    }
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        return s==t;
+};
+No results
