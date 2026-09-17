7c610bf31e1c191580ea52fc822b7d508f7f3c8f
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2026-09-17 22:37:14 +0530

    LeetCode: next-greater-element-i — solution file

---
+++ LeetCode/next-greater-element-i/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+       int m = nums1.size();
+       int n = nums2.size();
+       stack<int>st;
+       vector<int>v;
+       unordered_map<int,int>mp;
+       for(int i = n-1;i>=0;i--){
+           while(!st.empty() && st.top()<=nums2[i]) st.pop();
+           if(st.empty()) mp[nums2[i]] = -1;
+           else mp[nums2[i]] = st.top();
+           st.push(nums2[i]);
+       }
+       for (int i = 0; i < nums1.size(); i++) {
+           v.push_back(mp[nums1[i]]);
+       }
+       return v;
+   }
+};
