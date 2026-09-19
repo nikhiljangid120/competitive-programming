7380c1300e9b30566a5a730071425e80763ee045
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2026-09-19 23:56:14 +0530

    GFG: Infix to Postfix — solution file

---
+++ GFG/infix-to-postfix/solution.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int order(char ch){
+        int value = -1;
+        switch(ch){
+            case '^': value = 3; break;
+            case '/': value = 2; break;
+            case '*': value = 2; break;
+            case '+': value = 1; break;
+            case '-': value = 1; break;
+            default : value = -1; break;
+        }
+        return value;
+    }
+      string infixToPostfix(string& s) {
+          string str = "";
+          stack<char>st;
+          for(auto it: s){
+              if(it != '+' && it != '-' && it != '/' 
+                      && it != '*' && it != '(' && it != ')' && it != '^'){
+                  str += it;
+              }
+              else{
+                  if(it == '(') st.push(it);
+                  else if(it == ')'){
+                      while(!st.empty() && st.top() != '('){
+                          str += st.top();
+                          st.pop();
+                      }
+                      st.pop();
+                  }
+                  else{
