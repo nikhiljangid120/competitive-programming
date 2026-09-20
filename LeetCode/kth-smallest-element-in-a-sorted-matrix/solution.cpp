410227b31d93ee13a44cdb9404ca3c638d6d3966
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2026-09-20 23:54:21 +0530

    LeetCode: Kth Smallest Element in a Sorted Matrix — solution file

---
+++ LeetCode/kth-smallest-element-in-a-sorted-matrix/solution.cpp
@@ -0,0 +1,30 @@
+1
+2
+3
+4
+5
+6
+7
+8
+12
+9
+11
+14
+10
+13
+15
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        priority_queue<int>pq;
+        for(int i =0;i<m;i++){
+            for(int j = 0;j<n;j++){
+        }
+                pq.push(matrix[i][j]);
+            }
+    }
+                if(pq.size()>k) pq.pop();
+        return pq.top();
+};
