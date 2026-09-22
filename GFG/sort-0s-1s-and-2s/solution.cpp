9f4aec2f939d72579358d60f678236f5e1d040ad
Nikhil Jangid <161968345+nikhiljangid120@users.noreply.github.com>
2026-09-22 23:16:46 +0530

    GFG: Sort 0s, 1s and 2s — solution file

---
+++ GFG/sort-0s-1s-and-2s/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int n = arr.size();
+        int low = 0, mid = 0, high = n - 1;
+        while(mid<=high){
+            if(arr[mid]==0) swap(arr[low++], arr[mid++]);
+            else if(arr[mid]==1) mid++;
+            else swap(arr[mid], arr[high--]);
+        }
+    }
+};
